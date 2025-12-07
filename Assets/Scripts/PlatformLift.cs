97961daaec4ff5dd371fdd0c545addc62f15e0a6
Valeria Rincon <gguerrero@timberlea.cs.dal.ca>
2025-12-06 20:19:58 -0400

    Fixed platform lift

--- Assets/Scripts/PlatformLift.cs
+++ Assets/Scripts/PlatformLift.cs
@@ -3,15 +3,14 @@ using UnityEngine;
 public class PlatformLift : MonoBehaviour
 {
     public Transform destination;
-
     public float riseSpeed = 2f;
     public float rotateSpeed = 90f;
 
     private bool lifting = false;
     private Transform player;
-
-    public GameObject secondFloor;
     public Transform platform;
+    public GameObject playerDestination;
+    public GameObject secondFloor;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -24,31 +23,39 @@ public class PlatformLift : MonoBehaviour
 
     private void Update()
     {
-        if (!lifting || player == null || platform == null) return;
+        if (!lifting || player == null) return;
 
-        player.SetParent(platform);
-
-        platform.position = Vector3.MoveTowards(
-            platform.position,
+        player.position = Vector3.MoveTowards(
+            player.position,
             destination.position,
             riseSpeed * Time.deltaTime
         );
 
-        player.Rotate(Vector3.up * rotateSpeed * Time.deltaTime, Space.World);
-            player.SetParent(null);
+        if (platform != null)
+        {
+            platform.position = Vector3.MoveTowards(
+                platform.position,
+                destination.position,
+                riseSpeed * Time.deltaTime
+            );
+        }
 
-        if (Vector3.Distance(platform.position, destination.position) < 0.1f)
+        player.Rotate(Vector3.up * rotateSpeed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(player.position, destination.position) < 0.1f)
         {
             lifting = false;
 
-            player.position = new Vector3( Mathf.Round(destination.position.x), Mathf.Round(destination.position.y), Mathf.Round(destination.position.z) );
-            platform.gameObject.SetActive(false);
-
-            player.rotation = Quaternion.Euler(player.rotation.eulerAngles.x, 180, player.rotation.eulerAngles.z);
-
+            player.gameObject.SetActive(false);
             if (secondFloor != null)
             {
                 secondFloor.SetActive(true);
+                playerDestination.SetActive(true);
+            }
+            CameraControl cam = Camera.main.GetComponent<CameraControl>();
+            if (cam != null)
+            {
+                cam.player = playerDestination.transform;
             }
         }
     }
